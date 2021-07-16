958c594935a5a80d6e957123d335c83d7d289a06
coder966 <coder966@gmail.com>
2021-07-16 05:57:44 +0300

    type fixes

--- src/pageable-table/types/TableColumn.ts
+++ src/pageable-table/types/TableColumn.ts
@@ -1,9 +1,9 @@
 interface TableColumn {
   label: React.ReactNode;
   value: string | ((row: object) => React.ReactNode);
-  sortable: boolean;
-  sortKey: string;
-  display: boolean;
+  sortable?: boolean;
+  sortKey?: string;
+  display?: boolean;
 }
 
 export default TableColumn;
