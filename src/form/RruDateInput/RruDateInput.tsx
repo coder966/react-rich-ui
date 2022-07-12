f9947bfb93cf1ee53a45bd523f5b23f4778fd978
coder966 <coder966@gmail.com>
2022-07-12 21:28:40 +0300

    RruDateInputProps: breaking: render floating picker rather than 3 select elements

--- src/form/RruDateInput/RruDateInput.tsx
+++ src/form/RruDateInput/RruDateInput.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
-import DateTimePicker from '../common/DateTimePicker';
+import { addOrSubtractDays, isValidDateObject, range } from '../../utils/utils';
 import ErrorMessage from '../common/ErrorMessage';
 import Label from '../common/Label';
+import './style.css';
 
 export interface RruDateInputProps {
   /**  */
@@ -16,9 +17,6 @@ export interface RruDateInputProps {
 
   /**  */
   requiredAsterisk?: boolean;
-
-  /** yyyy-mm-dd */
-  defaultValue?: string;
 
   /**  */
   isHijri?: boolean;
@@ -40,26 +38,118 @@ export interface RruDateInputProps {
  * @author coder966
  */
 const RruDateInput: FC<RruDateInputProps> = (props) => {
-  const { name, disabled } = props;
-
   const formContext = useFormContext();
-  formContext.register({ name });
+
+  const [calendar, setCalendar] = useState<Date[]>();
+  const [isPopupShown, setIsPopupShown] = useState<boolean>(false);
+  const [year, setYear] = useState<number>(0);
+  const [month, setMonth] = useState<number>(0);
+  const [date, setDate] = useState<Date>();
+
+  const generateSixWeeksCalendar = (year: number, month: number): Date[] => {
+    const firstDayOfMonthDate = new Date(year, month - 1, 1);
+    const firstDayOfTheVisibleCalendar = addOrSubtractDays(firstDayOfMonthDate, -firstDayOfMonthDate.getDay());
+
+    const result = [firstDayOfTheVisibleCalendar];
+    for (let i = 1; i < 42; i++) { // 6 rows of 7 days each = 42 days
+      result.push(addOrSubtractDays(firstDayOfTheVisibleCalendar, i));
+    }
+
+    return result;
+  }
+
+  /**
+   * init
+   */
+  useEffect(() => {
+    // read the initial value
+    formContext.register({ name: props.name });
+    const initialValue = formContext.getValues()[props.name];
+    const initialDateObject = new Date(initialValue);
+
+    // determine the default date
+    let defaultSelectedDate;
+    if(isValidDateObject(initialDateObject)){
+      defaultSelectedDate = initialDateObject;
+    }else{
+      defaultSelectedDate = new Date();
+    }
+
+    // set state to the default date
+    setYear(defaultSelectedDate.getFullYear());
+    setMonth(defaultSelectedDate.getMonth() + 1);
+    setDate(defaultSelectedDate);
+  }, [])
+
+  useEffect(() => {
+    setCalendar(generateSixWeeksCalendar(year, month));
+  }, [year, month])
+
+  useEffect(() => {
+    const newValue = date?.toISOString().substring(0, 10);
+    console.log('date changed newValue = ', newValue);
+    formContext.setValue(props.name, newValue);
+  }, [date])
+
+  const getDayClassName = (date: Date): string => {
+    return `rru-date-input__day ${date.getMonth() + 1 === month ? '' : 'rru-date-input__day--grey'}`;
+  }
+
+  if (!calendar) {
+    return null;
+  }
 
   return (
     <div className='form-group'>
       <Label inputName={props.name} label={props.label} requiredAsterisk={props.requiredAsterisk} />
-      <DateTimePicker
-        type='date'
-        onChange={(value) => formContext.setValue(name, value)}
-        defaultValue={props.defaultValue}
-        reverseDisplayOrder={props.reverseDisplayOrder}
-        isHijri={props.isHijri}
-        isFuture={props.isFuture}
-        isPast={props.isPast}
-        maxYearLength={props.maxYearLength}
-        disabled={disabled}
-      />
-      <ErrorMessage inputName={name} />
+
+      <div className='rru-date-input'>
+
+        <input
+          type='text'
+          disabled={props.disabled}
+          value={date?.toISOString().substring(0, 10)}
+          onChange={e => {}}
+          onClick={e => setIsPopupShown(true)} className='form-control'
+        />
+
+        <div className={`rru-date-input__popup ${isPopupShown ? 'rru-date-input__popup--shown' : 'rru-date-input__popup--hidden'}`}>
+          <div className='rru-date-input__container'>
+
+            <div className='rru-date-input__header'>
+              <select value={year} onChange={e => setYear(parseInt(e.target.value))}>
+                {range(100, -50).map(i => <option key={`y${i}`}>{(new Date()).getFullYear() + i}</option>)}
+              </select>
+              <select value={month} onChange={e => setMonth(parseInt(e.target.value))}>
+                {range(12, 1).map(i => <option key={`m${i}`}>{i}</option>)}
+              </select>
+            </div>
+
+            <div className='rru-date-input__body'>
+              <table>
+                <tbody>
+                  {range(6).map(i => (
+                    <tr key={`r${i}`}>
+                      {range(7).map(j => {
+                        const index = ((i * 7) + j);
+                        const date = calendar[index];
+                        return <td key={`d${date.getTime()}`}>
+                          <div className={getDayClassName(date)} onClick={e => {
+                            setDate(date);
+                            setIsPopupShown(false);
+                          }}>{date.getDate()}</div>
+                        </td>
+                      })}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+      <ErrorMessage inputName={props.name} />
     </div>
   );
 };
