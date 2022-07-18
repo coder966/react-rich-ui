d561371414fa71a493737f154cbefb68546c72de
coder966 <coder966@gmail.com>
2022-07-19 02:08:36 +0300

    StoryBook: enhance stories

--- stories/RruStepsWizard.stories.tsx
+++ stories/RruStepsWizard.stories.tsx
@@ -8,37 +8,36 @@ const storyMeta: Meta = {
 
 export default storyMeta;
 
+const FirstStep = props => (
+  <div>
+    <h1>This is the first step content</h1>
+    <button onClick={props.nextStep}>next</button>
+    <button onClick={props.lastStep}>last step</button>
+  </div>
+);
+
+const SecondStep = props => (
+  <div>
+    <h1>This is the second step content</h1>
+    <button onClick={props.previousStep}>back</button>
+    <button onClick={event => props.nextStep({ name: 'Khalid', colour: 'Blue' })}>next</button>
+  </div>
+);
+
+const ThirdStep = props => (
+  <div>
+    <h1>This is the third step content</h1>
+    {props.previousStepData ?
+      <>
+        <p>Name: {props.previousStepData.name}</p>
+        <p>Colour: {props.previousStepData.colour}</p>
+      </>
+      : null}
+    <button onClick={props.firstStep}>first step</button>
+  </div>
+);
+
 export const WizardExample = (args) => {
-
-  const FirstStep = props => (
-    <div>
-      <h1>This is the first step content</h1>
-      <button onClick={props.nextStep}>next</button>
-      <button onClick={props.lastStep}>last step</button>
-    </div>
-  );
-
-  const SecondStep = props => (
-    <div>
-      <h1>This is the second step content</h1>
-      <button onClick={props.previousStep}>back</button>
-      <button onClick={event => props.nextStep({ name: 'Khalid', colour: 'Blue' })}>next</button>
-    </div>
-  );
-
-  const ThirdStep = props => (
-    <div>
-      <h1>This is the third step content</h1>
-      {props.previousStepData ?
-        <>
-          <p>Name: {props.previousStepData.name}</p>
-          <p>Colour: {props.previousStepData.colour}</p>
-        </>
-        : null}
-      <button onClick={props.firstStep}>first step</button>
-    </div>
-  );
-
   return (
     <RruStepsWizard>
       <FirstStep stepLabel='First Step' />
