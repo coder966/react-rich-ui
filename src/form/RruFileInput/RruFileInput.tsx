29546a37a3648acb299c9061a7d5c9a1cb0a1db3
coder966 <coder966@gmail.com>
2022-07-21 22:41:54 +0300

    RruFileInput: migrate to bootstrap v5

--- src/form/RruFileInput/RruFileInput.tsx
+++ src/form/RruFileInput/RruFileInput.tsx
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React, { FC, useEffect, useRef, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useFormContext } from 'react-hook-form';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import Label from '../Label/Label';
@@ -22,24 +22,20 @@ import './style.css';
 import RruFileInputProps from './types/RruFileInputProps';
 
 const RruFileInput: FC<RruFileInputProps> = (props) => {
-  const [fileName, setFileName] = useState<string | null>(null);
   const formContext = useFormContext();
-  const ref = useRef<HTMLInputElement>(null);
 
   // init
   useEffect(() => {
     formContext.register({ name: props.name });
-    formContext.setValue(props.name, null);
+    formContext.setValue(props.name, undefined);
   }, []);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const filesList = e.target.files;
     if (filesList && filesList[0]) {
-      setFileName(filesList[0].name);
       formContext.setValue(props.name, filesList[0]);
     } else {
-      setFileName(null);
-      formContext.setValue(props.name, null);
+      formContext.setValue(props.name, undefined);
     }
   }
 
@@ -47,23 +43,13 @@ const RruFileInput: FC<RruFileInputProps> = (props) => {
     <div className='form-group'>
       <Label inputName={props.name} label={props.label} requiredAsterisk={props.requiredAsterisk} />
       <input
+        aria-label='Upload'
         {...props}
-        className='rru-file-input__input'
         type='file'
-        ref={ref}
+        className='form-control'
         name={props.name}
         onChange={onChange}
       />
-      <div
-        className={`form-control rru-file-input__visible-control ${formContext.errors[props.name] ? 'is-invalid' : ''}`}
-        onClick={(e) => {
-          if (ref.current) {
-            ref.current.click();
-          }
-        }}
-      >
-        {fileName || props.placeholder}
-      </div>
       <ErrorMessage inputName={props.name} />
     </div>
   );
