eef9f55ea4f85ba797cfaebd61ed054db615cf97
coder966 <coder966@gmail.com>
2022-07-21 19:45:03 +0300

    storybook: load bootstrap css and js from npm package

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
