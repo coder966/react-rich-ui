3d4bc46c28ebe7ef5c5d4cb40e4b8918420aa4f7
coder966 <coder966@gmail.com>
2022-07-31 12:39:00 +0300

    RruTextInput and RruTextareaInput: fix not working properly when page dir is different from input dir

--- src/form/RruForm/RruForm.tsx
+++ src/form/RruForm/RruForm.tsx
@@ -17,6 +17,7 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import React, { FC } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
+import './style.css';
 import RruFormProps from './types/RruFormProps';
 
 const RruForm: FC<RruFormProps> = (props) => {
