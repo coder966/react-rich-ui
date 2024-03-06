deaceef6b46dc1d915f542646f46cd8c4b623cc7
Khalid Alharisi <coder966@users.noreply.github.com>
2024-03-06 10:43:52 +0000

    migrate to storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,21 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
   },
-  reactOptions: {
-    strictMode: true,
+
+  docs: {
+    autodocs: true
   },
+
+  framework: {
+    name: '@storybook/react-webpack5',
+
+    options: {
+      strictMode: true
+    }
+  }
 };
