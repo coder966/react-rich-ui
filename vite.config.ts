4d86771007c679648ff4f328434dd6ca33b8bf25
coder966 <coder966@gmail.com>
2024-03-22 01:31:27 +0300

    exclude dependencies from bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+// @ts-expect-error no declaration file
+import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['src'] })],
+  plugins: [react(), dts({ include: ['src'] }), excludeDependenciesFromBundle()],
   build: {
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
       output: {
         entryFileNames: '[name].js',
       },
