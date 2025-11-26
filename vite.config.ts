15c21960a4c51fc6bd360e1409609c962864da50
coder966 <coder966@gmail.com>
2025-11-26 06:09:05 +0300

    upgrade vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-// @ts-expect-error no declaration file
 import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts({ include: ['src'] }), excludeDependenciesFromBundle()],
   build: {
@@ -13,6 +11,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.tsx'),
       formats: ['cjs', 'es'],
+      cssFileName: 'style',
     },
   },
 });
